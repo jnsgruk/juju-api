e40ebec187bbc45d8ccc09e6e3dd0774fd3688c0
Jon Seager <jon@sgrs.uk>
2023-06-30 11:34:10 +0100

    feat: add flake.nix for easier 'nix develop' usage

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "juju api";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      forAllSystems = inputs.nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShell {
+          # Enable experimental features without having to specify the argument
+          NIX_CONFIG = "experimental-features = nix-command flakes";
+          nativeBuildInputs = with pkgs; [
+            nodejs
+          ];
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin/:$PATH"
+          '';
+        }
+      );
+    };
+}
